2745f194cec6216d506074386eee6937b01dfeca
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-05-28 21:39:38 -0700

    Add files via upload

--- Single Number II.py
+++ Single Number II.py
@@ -11,3 +11,13 @@ class Solution:
         for k, v in count.items():
             if v == 1:
                 return k
+
+        # Solution 2
+        single = 0
+        for i in range(32):
+            count = 0
+            for num in nums:
+                if num & (1 << i) == (1 << i):
+                    count += 1
+            single |= (count % 3) << i
+        return single if single < (1 << 31) else single - (1 << 32)
